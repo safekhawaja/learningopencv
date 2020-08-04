5da6be1917553ee8023044d0af285b0f4fd61700
safekhawaja <68385788+safekhawaja@users.noreply.github.com>
2020-08-04 18:17:45 -0400

    s

--- opencvtest/warp.py
+++ opencvtest/warp.py
@@ -3,9 +3,13 @@ import numpy as np
 
 img = cv2.imread("Resources/lads.png")
 
+width,height = 250,300
+pts1 = np.float32([[420,440],[610,620],[420,620],[440,610]])
+pts2 = np.float32([[0,0],[width,height],[0,width],[height,0]])
 
+matrix = cv2.getPerspectiveTransform(pts1,pts2)
+imgOutput = cv2.warpPerspective(img,matrix,(width,height))
 
-
-cv2.imshow("Image",img)
-
+cv2.imshow("Image", img)
+cv2.imshow("Output", imgOutput)
 cv2.waitKey(0)
